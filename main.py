8f551a3c1aee6a2c85c5031ffcdca0117d48a988
_Marcuth <91915075+1Marcuth@users.noreply.github.com>
2022-01-01 20:44:56 -0300

    Adição de Novos Arquivos

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from heroic_race_crawler import crawler
 
 api = FastAPI()
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @api.get('/laps')
 def laps():
